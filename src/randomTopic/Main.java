bb22b1192869772d4cbc1b9a83d4c74e1c71b765
kaizen-98 <ammalnassri@gmail.com>
2022-12-25 03:59:39 -0800

    Constrcture conpct add class

---
+++ src/randomTopic/Main.java
@@ -0,0 +1,14 @@
+package randomTopic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Person person = new Person("Mohamed",24);
+		System.out.println(person.name);
+		System.out.println(person.age);
+	}
+
+}
---
+++ src/randomTopic/Person.java
@@ -0,0 +1,12 @@
+package randomTopic;
+
+public class Person {
+
+	String name;
+	int age;
+	public Person(String name,int age) {
+		this.name = name;
+		this.age = age;
+		
+	}
+}
