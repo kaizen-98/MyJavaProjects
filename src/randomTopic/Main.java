f8e71085f8dfafcd95d86f12308159c447355909
kaizen-98 <ammalnassri@gmail.com>
2022-12-25 04:45:33 -0800

    Here new comment

--- src/randomTopic/Main.java
+++ src/randomTopic/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		
+		// passing the parmeter here
 		Person person = new Person("Mohamed",24);
+		// printing
 		System.out.println(person.name);
 		System.out.println(person.age);
 	}
--- src/randomTopic/Person.java
+++ src/randomTopic/Person.java
@@ -2,8 +2,11 @@ package randomTopic;
 
 public class Person {
 
+	
+	// here we puts varbiale
 	String name;
 	int age;
+	//create consterueione
 	public Person(String name,int age) {
 		this.name = name;
 		this.age = age;
